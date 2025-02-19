7d0c64fd343a3e5a508cf11947e14262620eed4d
Juan Jose Cetraro <jjcetraro@gmail.com>
2025-02-18 23:39:17 -0300

    feat: change AGL text with AMSL

--- tokyo/src/converters/core/position/head_label.ts
+++ tokyo/src/converters/core/position/head_label.ts
@@ -22,7 +22,7 @@ const getConverterFromPosition: VehiclePositionHeadLabelTokyoConverter['getConve
 				d.location.coordinates[1],
 				d.altitude_gps * ELEVATION_MULTIPLIER
 			],
-			getText: (d) => `${d.altitude_gps}m AGL`,
+			getText: (d) => `${d.altitude_gps}m AMSL`,
 			getSize: options.size,
 			getTextAnchor: 'start',
 			getAlignmentBaseline: 'top',
